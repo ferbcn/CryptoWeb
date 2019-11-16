4baa0754d49583ab0003aabbd5605a5470ca4fd3
ferbcn <17597019+ferbcn@users.noreply.github.com>
2019-11-16 12:17:53 +0100

    static files

--- mysite/settings.py
+++ mysite/settings.py
@@ -173,5 +173,3 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'cryptoweb/static'),)
-
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
